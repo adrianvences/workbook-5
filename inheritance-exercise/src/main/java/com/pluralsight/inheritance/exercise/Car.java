5436da130342402949fb6b7863c35102e73c365d
adrianvences9820 <adrianvences9820@gmail.com>
2024-10-29 16:55:31 -0400

    inheritance exercise added some getters

--- inheritance-exercise/src/main/java/com/pluralsight/inheritance/exercise/Car.java
+++ inheritance-exercise/src/main/java/com/pluralsight/inheritance/exercise/Car.java
@@ -4,8 +4,12 @@ public class Car extends Vehicle{
     private int numberOfDoors;
 
 
-    public Car(String make, String model, int year, int numberOfDoors) {
-        super(make, model, year);
+    public Car(String make, String model, int year, int numberOfSeats,int numberOfDoors) {
+        super(make, model, year,numberOfSeats);
         this.numberOfDoors = numberOfDoors;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
     }
 }
--- inheritance-exercise/src/main/java/com/pluralsight/inheritance/exercise/Vehicle.java
+++ inheritance-exercise/src/main/java/com/pluralsight/inheritance/exercise/Vehicle.java
@@ -4,10 +4,28 @@ public class Vehicle {
     private String make;
     private String model;
     private int year;
+    private int numberOfSeats;
 
-    public Vehicle(String make, String model, int year) {
+    public Vehicle(String make, String model, int year, int numberOfSeats) {
         this.make = make;
         this.model = model;
         this.year = year;
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
     }
 }
